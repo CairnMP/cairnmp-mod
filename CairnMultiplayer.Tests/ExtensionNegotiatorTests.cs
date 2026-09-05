82691ea601d291cac09bffca29b3c88b312feb7e
Yutho <101727352+yutho-o@users.noreply.github.com>
2026-09-06 01:15:18 +0200

    test: consolidate portable suites and remove duplicate test compilation

--- CairnMultiplayer.Tests/ExtensionNegotiatorTests.cs
+++ CairnMultiplayer.Tests/ExtensionNegotiatorTests.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using System.IO;
 using CairnMultiplayer.Shared;
 using CairnMultiplayer.Shared.Extensions;
--- CairnMultiplayer.Tests/PacketValidationTests.cs
+++ CairnMultiplayer.Tests/PacketValidationTests.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using CairnMultiplayer.Shared;
 using Xunit;
 
